478e68c026c260e555813af9924827f4825f4dab
KaikyALB <kaikyalbuquerque16@gmail.com>
2025-12-07 19:48:44 -0500

    Player Insertion

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private CharacterController cc;
+
+    [Header("Basic Stats")]
+    private float Horizontal;
+    public float speed = 3f;
     void Start()
     {
-        
+    rb = GetComponent<Rigidbody>();
+    cc = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        float Horizontal = Input.GetAxis("Horizontal"); // Moves Sideways
+
+        Movement();
+    }
+    public void Movement()
+    {
+        Vector3 move = transform.position * Horizontal;
+        cc.Move(move * speed * Time.deltaTime);
+
         
     }
 }
